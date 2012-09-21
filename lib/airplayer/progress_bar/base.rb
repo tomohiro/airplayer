51cdb0ed076b8e6e241af7cc46c3a6cb12ea080a
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-09-21 12:51:55 +0900

    fix bug: failed update total

--- lib/airplayer/progress_bar/base.rb
+++ lib/airplayer/progress_bar/base.rb
@@ -3,7 +3,7 @@ require 'ruby-progressbar'
 class ProgressBar
   class Base
     def total=(new_total)
-      with_update { @bar.total = new_total }
+      with_update { with_progressables(:total=, new_total) }
     end
   end
 end
