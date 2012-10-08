0182f86b93dc8e145c611e1708fe19e354cea000
Yusaku ONO <yono.05@gmail.com>
2012-10-08 22:15:16 +0900

    add shuffle option when play dir

--- lib/airplayer/app.rb
+++ lib/airplayer/app.rb
@@ -2,11 +2,12 @@ require 'thor'
 
 module AirPlayer
   class App < Thor
-    desc 'play <URI|FILE|DIR> [-r|--repeat] ', 'Play video(URI or local video file path or video directory)'
+    desc 'play <URI|FILE|DIR> [-r|--repeat] [-s|--shuffle]', 'Play video(URI or local video file path or video directory)'
     method_option :repeat, :aliases => '-r', :desc => 'Repeat play', :type => :boolean
+    method_option :shuffle, :aliases => '-s', :desc => 'Random play', :type => :boolean
     def play(target)
       controller = Controller.new
-      Playlist.new.add(target).entries(options.repeat) do |media|
+      Playlist.new.add(target, options.shuffle).entries(options.repeat) do |media|
         controller.play(media)
       end
     end
