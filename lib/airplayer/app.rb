f7ff0116b9654cacef1872afe2723bda6e0d9c4b
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-01-09 21:51:43 +0900

    1.9.3 style

--- lib/airplayer/app.rb
+++ lib/airplayer/app.rb
@@ -3,8 +3,8 @@ require 'thor'
 module AirPlayer
   class App < Thor
     desc 'play <URI|Podcast XML|FILE|DIR> [-r|--repeat] [-s|--shuffle]', 'Play video(URI or Podcast XML URI or local video file path or video directory)'
-    method_option :repeat,  :aliases => '-r', :desc => 'Repeat play',  :type => :boolean
-    method_option :shuffle, :aliases => '-s', :desc => 'Shuffle play', :type => :boolean
+    method_option :repeat,  aliases: '-r', desc: 'Repeat play',  type: :boolean
+    method_option :shuffle, aliases: '-s', desc: 'Shuffle play', type: :boolean
     def play(target)
       controller = Controller.new
       Playlist.new(options).add(target).entries do |media|
