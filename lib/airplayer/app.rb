b7e5b485781dd6f2519bea8eb24b2c4108325b79
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-23 18:07:47 +0900

    add device options

--- lib/airplayer/app.rb
+++ lib/airplayer/app.rb
@@ -3,13 +3,19 @@ require 'thor'
 module AirPlayer
   class App < Thor
     desc 'play <URI|Podcast XML|FILE|DIR> [-r|--repeat] [-s|--shuffle]', 'Play video(URI or Podcast XML URI or local video file path or video directory)'
-    method_option :repeat,  aliases: '-r', desc: 'Repeat play',  type: :boolean
-    method_option :shuffle, aliases: '-s', desc: 'Shuffle play', type: :boolean
+    method_option :repeat,  aliases: '-r', desc: 'Repeat play',   type: :boolean
+    method_option :shuffle, aliases: '-s', desc: 'Shuffle play',  type: :boolean
+    method_option :device,  aliases: '-d', desc: 'Device number', type: :numeric
     def play(target)
       controller = Controller.new
       Playlist.new(options).add(target).entries do |media|
         controller.play(media)
       end
+    end
+
+    desc 'devices', 'Show AirPlay devices'
+    def devices
+      Device.new.list
     end
 
     map '--version' => :version
