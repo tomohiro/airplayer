214d48b8e78a206bf34d6c60af6c3c5dc3d508d2
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-12-29 21:04:24 +0900

    Define dummy device to test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
-require 'airplayer'
-
 require 'coveralls'
 Coveralls.wear!
+
+require 'airplayer'
+require 'airplay'
+
+Airplay.configure do |c|
+  c.autodiscover = false
+end
+
+Airplay.devices.add('Dummy Device', 'dummy.appletv.local:7000')
+
+def dummy_device
+  Airplay['Dummy Device']
+end
