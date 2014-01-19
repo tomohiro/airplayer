b2c94bdea629a395ac20b68f719c7ee3295be691
Tomohiro TAIRA <tomohiro.t@gmail.com>
2014-01-19 21:36:53 +0900

    Add spec to play local directory

--- spec/airplayer/playlist_spec.rb
+++ spec/airplayer/playlist_spec.rb
@@ -1,3 +1,4 @@
+require 'fakefs/spec_helpers'
 require 'spec_helper'
 
 module AirPlayer
@@ -7,8 +8,22 @@ module AirPlayer
     end
 
     describe '.add' do
+      context 'with local directory' do
+        it 'returns media type is local file' do
+          FakeFS do
+            FileUtils.touch('01.m4v')
+            FileUtils.touch('02.m4v')
+          end
+
+          playlist.add('.')
+          playlist.entries do |media|
+            expect(media.file?).to be true
+          end
+        end
+      end
+
       context 'with URL' do
-        it 'return media type is url' do
+        it 'returns media type is url' do
           playlist.add('http://example.com/video.mp4')
           expect(playlist.first.path).to match 'http'
         end
@@ -16,8 +31,8 @@ module AirPlayer
 
       context 'with multiple files' do
         it 'have multiple files' do
-          expect(playlist.add('./video.mp4').size).to eq 1
-          expect(playlist.add('./video.m4v').size).to eq 2
+          expect(playlist.add('video.mp4').size).to eq 1
+          expect(playlist.add('video.m4v').size).to eq 2
         end
       end
 
@@ -32,7 +47,7 @@ module AirPlayer
 
       context 'with local file' do
         it 'returns media instances' do
-          playlist.add('../video.mp4')
+          playlist.add('video.mp4')
           playlist.entries do |media|
             expect(media).to be_kind_of AirPlayer::Media
           end
